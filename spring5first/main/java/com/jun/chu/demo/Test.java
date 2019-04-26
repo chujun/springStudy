1540a11b32f962120b31fb9416ec80a03c9c8382
za-chujun <1571970439@qq.com>
2019-04-26 11:06:08 +0800

    init spring 5 first

---
+++ spring5first/main/java/com/jun/chu/demo/Test.java
@@ -0,0 +1,11 @@
+package com.jun.chu.demo;
+
+/**
+ * @author jun.chu
+ * @date 2019-04-26 11:05
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("1");
+    }
+}
