cb557d55548fcfa8149e71f9d8a3066e6e68673f
za-chujun <1571970439@qq.com>
2019-04-26 17:16:29 +0800

    resources文件夹中的配置文件都编译不到classes

--- spring5first/main/java/com/jun/chu/demo/core1i3/Application.java
+++ spring5first/main/java/com/jun/chu/demo/core1i3/Application.java
@@ -1,7 +1,5 @@
 package com.jun.chu.demo.core1i3;
 
-import com.jun.chu.demo.core1i3.ClientService;
-import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,8 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @date 2019-04-26 16:28
  */
 public class Application {
-    @Test
-    public void factoryMethod() {
+    public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.xml");
 
         ClientService clientService = (ClientService) applicationContext.getBean("clientService");
